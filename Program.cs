296843efe853ccb345b32a72f9e7f9e954b390ff
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-29 12:14:28 +0100

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vektorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vektor = new int[10]; //en vektor med 10 st "platser"
+
+
+
+
+
+
+            Console.WriteLine("press any key to exit");//utskrift
+            Console.ReadKey();
+
+        }
+    }
+}
